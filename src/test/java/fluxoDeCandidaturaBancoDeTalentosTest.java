bb161b7b8697400fe5f6c17092ad487f81c5862b
Tairine <t.mousinho.de.lima@avanade.com>
2021-02-07 14:56:07 -0300

    Acesso ao portal do usuario já cadastrado

--- src/test/java/fluxoDeCandidaturaBancoDeTalentosTest.java
+++ src/test/java/fluxoDeCandidaturaBancoDeTalentosTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -11,39 +12,54 @@ import java.util.concurrent.TimeUnit;
 
 public class fluxoDeCandidaturaBancoDeTalentosTest {
     private WebDriver navegador;
+
     @Before
     public void setUP(){
     System.setProperty("webdriver.chrome.driver", "C:\\Users\\t.mousinho.de.lima\\drives\\chromedriver.exe");
     navegador = new ChromeDriver();
     navegador.get("https://nayaracorporation.solides.jobs/");
     navegador.manage().window().maximize();
-    navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    navegador.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
+    navegador.findElement(By.id("hs-eu-confirmation-button")).click();
+
     }
 
-    @Test
-    public void testCandidaturaAoBancoDeTalentos(){
+    //@Test
+    public void testCadastroDeCandidato(){
 
         // Acessar formulário
-        navegador.findElement(By.id("hs-eu-confirmation-button")).click();
         navegador.findElement(By.id("registerTalentBank")).click();
         navegador.findElement(By.id("register")).click();
 
         // Dados do formulário
-        navegador.findElement(By.xpath("//form//div//div//div//input[@name='email']")).sendKeys("teste@teste.com.br");
-        navegador.findElement(By.xpath("//form//div//div//div//input[@name='cpf']")).sendKeys("32841137031");
-        navegador.findElement(By.xpath("//form/div[3]/div/div/input")).sendKeys("123456");
-        navegador.findElement(By.xpath("//form/div[4]/div/div/input")).sendKeys("123456");
+        navegador.findElement(By.xpath("//form//div//div//div//input[@name='email']")).sendKeys("teste2@teste.com.br");
+        navegador.findElement(By.xpath("//form//div//div//div//input[@name='cpf']")).sendKeys("53046836090");
+        navegador.findElement(By.xpath("//form/div[3]/div/div/input")).sendKeys("T124476");
+        navegador.findElement(By.xpath("//form/div[4]/div/div/input")).sendKeys("T124476");
         navegador.findElement(By.xpath("//form/div[5]/div/div/div/div/label")).click();
 
+        //Finalizar cadastro
+        navegador.findElement(By.xpath("//form/div[6]/button[@type='submit']")).click();
 
+        WebElement PaginaDeInformacoes = navegador.findElement(By.xpath("//div[1]/p/strong[text()=' preencha duas etapas:']"));
+        String MSG = PaginaDeInformacoes.getText();
+        Assert.assertEquals("preencha duas etapas:",MSG);
 
+    }
 
-        //WebDriverWait AguardarForm = new WebDriverWait(navegador, 10);
-        //AguardarForm.until(ExpectedConditions.visibilityOf((name))).click();
+    @Test
+    public void testAcessarPaginaDepoisDeCadastro(){
+        navegador.findElement(By.xpath("//div/div[1]/div/div[3]/div[2]/button")).click();
 
+        navegador.findElement(By.xpath("//form/div[1]/div/div/input[@name='user']")).sendKeys("teste1@teste.com.br");
+        navegador.findElement(By.xpath("//div/div[2]/div[2]/div/form/div[2]/div/div/input")).sendKeys("T124456");
+        navegador.findElement(By.xpath("//div/div[2]/div[2]/div/form/div[4]/button")).click();
 
-
-                //sendKeys("teste@teste.com.br");
+        WebElement PaginaDeInformacoes = navegador.findElement(By.xpath("//div[1]/p/strong[text()=' preencha duas etapas:']"));
+        String MSG = PaginaDeInformacoes.getText();
+        Assert.assertEquals("preencha duas etapas:",MSG);
 
     }
 }
